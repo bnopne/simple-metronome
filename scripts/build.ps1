9a4191f5e996c8e854df0b6b0856c44bfedaa095
bnopne <bnopne@gmail.com>
2019-02-23 16:11:35 +0300

    Fixup build script

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,16 +1,16 @@
 $electronDistURL = "https://github.com/electron/electron/releases/download/v4.0.4/electron-v4.0.4-win32-x64.zip"
-$electronArchive = ".\electron.zip"
-$appDistFolder = ".\dist"
-$appResourcesFolder = ".\dist\resources\app"
-$bundleContents = "..\dist"
-$package = "..\package.json"
-$appStartScript = "..\src\start-electron.js"
+$electronBinaryArchive = Join-Path -Path $PSScriptRoot -ChildPath "electron-v4.0.4-win32-x64.zip"
+$appDistFolder = Join-Path -Path $PSScriptRoot -ChildPath "dist"
+$appResourcesFolder = Join-Path -Path $PSScriptRoot -ChildPath "dist\resources\app"
+$appBundle = Join-Path -Path $PSScriptRoot -Resolve "..\dist"
+$package = Join-Path -Path $PSScriptRoot -Resolve "..\package.json"
+$appStartScript = Join-Path -Path $PSScriptRoot -Resolve "..\src\start-electron.js"
 
 mkdir $appDistFolder
 
-Invoke-WebRequest -Uri $electronDistURL -OutFile $electronArchive
-Expand-Archive -Path $electronArchive -DestinationPath $appDistFolder
+Invoke-WebRequest -Uri $electronDistURL -OutFile $electronBinaryArchive
+Expand-Archive -Path $electronBinaryArchive -DestinationPath $appDistFolder
 
-Copy-Item -Path $bundleContents -Destination $appResourcesFolder -Recurse
+Copy-Item -Path $appBundle -Destination $appResourcesFolder -Recurse
 Copy-Item -Path $package -Destination $appResourcesFolder
 Copy-Item -Path $appStartScript -Destination $appResourcesFolder
